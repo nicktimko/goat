808688f45aab76e694584333c207b06479ee40c1
Nick Timkovich <prometheus235@gmail.com>
2015-11-17 19:22:24 -0600

    Clean up URLs

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from lists import views
+import lists.urls
+import lists.views
 
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$', lists.views.home_page, name='home'),
+    url(r'^lists/', include(lists.urls)),
 ]
