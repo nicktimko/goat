ac0c7a5c6461fed0386086e1cd11e6df922a3ba9
Nick Timkovich <prometheus235@gmail.com>
2015-11-17 20:17:19 -0600

    Add some je ne sais quoi (i.e. Bootstrap)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,16 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         # RAGNAROK INVOKED
+        self.browser.refresh() # shut up Windows
         self.browser.quit()
 
     def assertRowsInTable(self, expected_texts):
