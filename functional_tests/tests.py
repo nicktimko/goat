150a471458c8abc1cc37fa612598598c3f1a65ce
Nick Timkovich <prometheus235@gmail.com>
2015-11-17 19:50:37 -0600

    Func test styling/layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,3 +97,25 @@ class NewVisitorTest(LiveServerTestCase):
         ])
 
         # satisfied, they both invoke ragnarok.
+
+    def test_layout_and_styling(self):
+        # shelly goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=20
+        )
+
+        # input on the add-an-item page is also good
+        inputbox.send_keys('buy beer\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=20
+        )
